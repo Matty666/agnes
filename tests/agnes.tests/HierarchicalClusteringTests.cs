df953d81c873765a07907de3a5f1d0833a63f8ac
Matt Evans <matt@mattevans.email>
2019-03-17 18:41:34 +1100

    Added Log10 distance function test

--- tests/agnes.tests/HierarchicalClusteringTests.cs
+++ tests/agnes.tests/HierarchicalClusteringTests.cs
@@ -70,6 +70,14 @@ namespace agnes.tests
         {
             yield return new object[]
             {
+                TestClusterCandidate.ToTestList(1, 2, 3, 10, 99, 500, 1000, 5000, 9000, 9001, 9002, 9003, 9004),
+                1,
+                LinkageFunctions.CompleteLinkage((TestClusterCandidate i1, TestClusterCandidate i2) => Math.Abs(Math.Log10(i1.Value) - Math.Log10(i2.Value))),
+                TestClusterCandidate.ToClusterSets(new[] {1, 2, 3}, new[] {10, 99}, new[] {500, 1000}, new[] {5000, 9000, 9001, 9002, 9003, 9004})
+            };
+
+            yield return new object[]
+            {
                 TestClusterCandidate.ToTestList(10, 20, 499, 501, 700, 710),
                 200,
                 TestClusterCandidate.CompleteLinkage,
@@ -110,7 +118,7 @@ namespace agnes.tests
 
             yield return new object[]
             {
-                TestClusterCandidate.ToTestList(10, 20, 499, 501, 700, 710),
+                TestClusterCandidate.ToTestList(700, 499, 20, 501, 10, 710),
                 200,
                 TestClusterCandidate.SingleLinkage,
                 TestClusterCandidate.ToClusterSets(new[] {10, 20}, new[] {499, 501, 700, 710})
