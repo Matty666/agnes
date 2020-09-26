4f96cf282ce193a5bc7b07db338dfce61d1e174d
Matt Evans <matt@mattevans.email>
2020-09-26 10:40:04 +1000

    Add clustered enumerable

---
+++ src/agnes/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using agnes.Enumerable;
+
+namespace agnes
+{
+    public static class Extensions
+    {
+        public static readonly Func<double, double, double> CompleteLinkage = Math.Max;
+        public static readonly Func<double, double, double> SingleLinkage = Math.Min;
+
+        public static IClusteredEnumerable<TElement> ClusterBy<TElement>(this IEnumerable<TElement> source, Func<TElement, double> distanceValueSelector, double clusterDistanceCutPoint)
+        {
+            double DistanceFunction(TElement left, TElement right) => Math.Abs(distanceValueSelector(left) - distanceValueSelector(right));
+
+            return new ClusteredEnumerable<TElement>(source, DistanceFunction, CompleteLinkage, clusterDistanceCutPoint, distanceValueSelector);
+        }
+    }
+}
---
+++ tests/agnes.tests/ClusteredEnumerableTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace agnes.tests
+{
+
+    public class ClusteredEnumerableTests
+    {
+        [Fact]
+        public void Cluster()
+        {
+            var source = new List<int> { 101, 204, 203, 105, 103 };
+            var results = source.ClusterBy(v => v % 100, 2).ToList();
+            results.Count.ShouldBe(2);
+            results.First().ShouldBe(new List<int> {204, 105});
+            results.Last().ShouldBe(new List<int> {101, 203, 103});
+        }
+    }
+}
